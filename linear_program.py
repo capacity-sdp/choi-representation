3edb6d20d4e09322e4680d4921926f3b5457a5aa
yuxuanjerrychen01 <yuxuanjerrychen01@163.com>
2022-04-30 20:57:55 -0400

    finished werner lp

--- linear_program.py
+++ linear_program.py
@@ -55,14 +55,50 @@ def plot_lp_depol(dimension):
     print(end - start)
 
 
-# print(plot_lp_depol(3))
+def plot_lp_wern(dimension):
+    start = time.time()
+    d = dimension
+    c = []
+
+    q_vals = np.arange(d / (d + 1), d / (d - 1), 0.01, dtype=float)
+
+    r1 = cp.Variable(1)
+    r2 = cp.Variable(1)
+    r3 = cp.Variable(1)
+    r4 = cp.Variable(1)
+    mu = cp.Variable()
+
+    for q in q_vals:
+        constraints = [r1 >= 0, r3 >= 0]
+        constraints += [r1 + d * r2 >= 0, r3 + d * r4 >= 0]
+        constraints += [(d * (r1 + r3) + r2 + r4) <= mu]
+        constraints += [r1 + r2 - r3 - r4 + q - q / d - 1 >= 0]
+        constraints += [r1 + r4 - r2 - r3 - q / d + 1 - q >= 0]
+        prob = cp.Problem(cp.Minimize(mu), constraints)
+        prob.solve()
+        c.append(log2(prob.value))
+
+    plt.plot(q_vals, c, label="d = " + str(d))
+    plt.legend(loc="upper left")
+
+    plt.title('Werner-Holevo Channel with d=' + str(d))
+    plt.xlabel('q-parameter')
+    plt.ylabel('quantum capacity')
+    plt.savefig('./plot/werner/lp_wern_' + str(d) + '.png')
+
+    # plt.show()
+    end = time.time()
+    print(end - start)
+
+
+# print(plot_lp_wern(3))
 
 
 # plot a general plot of capacity bound of depolar/werner channels with a given lower/upper d
 def plot_all_lp_depol(upper):
     start = time.time()
     listt = [2, 10]
-    listt += list(np.arange(20, upper+1, 20))
+    listt += list(np.arange(20, upper + 1, 20))
     for i in (listt):
         d = i
         c = []
@@ -92,14 +128,60 @@ def plot_all_lp_depol(upper):
         else:
             plt.plot(t, c)
 
-        plt.title('Depolarizing Channel with d from ' + str("10") + ' to ' + str(upper))
+        plt.title('Depolarizing Channel with d from ' + str("2") + ' to ' + str(upper))
         # plt.xlabel('q-parameter')
         plt.xlabel('t-parameter')
         plt.ylabel("log" + r'$\Gamma(N)$')
-        plt.savefig('./plot/depol/lp_depol_' + str("10") + '_' + str(upper) + '.png')
+        plt.savefig('./plot/depol/lp_depol_' + str("2") + '_' + str(upper) + '.png')
 
         # plt.show()
     end = time.time()
     print(end - start)
+
+
+# plot a general plot of capacity bound of depolar/werner channels with a given lower/upper d
+def plot_all_lp_wern(upper):
+    start = time.time()
+    listt = [2, 5, 10]
+    listt += list(np.arange(20, upper + 1, 20))
+    for i in listt:
+        d = i
+        c = []
+        t = np.arange(0, 1, 0.01, dtype=float)
+        q_vals = t * (d / (d + 1)) + (1 - t) * (d / (d - 1))
+
+        r1 = cp.Variable(1)
+        r2 = cp.Variable(1)
+        r3 = cp.Variable(1)
+        r4 = cp.Variable(1)
+        mu = cp.Variable()
+
+        for q in q_vals:
+            constraints = [r1 >= 0, r3 >= 0]
+            constraints += [r1 + d * r2 >= 0, r3 + d * r4 >= 0]
+            constraints += [(d * (r1 + r3) + r2 + r4) <= mu]
+            constraints += [r1 + r2 - r3 - r4 + q - q / d - 1 >= 0]
+            constraints += [r1 + r4 - r2 - r3 - q / d + 1 - q >= 0]
+            prob = cp.Problem(cp.Minimize(mu), constraints)
+            prob.solve()
+            c.append(log2(prob.value))
+
+        # plt.plot(q_vals, c)
+        if d <= 40 or d == 100:
+            plt.plot(t, c, label="d = " + str(d))
+            plt.legend(loc="upper right")
+        else:
+            plt.plot(t, c)
+
+        plt.title('Werner-Holevo Channel with d from ' + str("2") + ' to ' + str(upper))
+        # plt.xlabel('q-parameter')
+        plt.xlabel('t-parameter')
+        plt.ylabel("log" + r'$\Gamma(N)$')
+        plt.savefig('./plot/werner/lp_wern_' + str("2") + '_' + str(upper) + '.png')
+
+        # plt.show()
+    end = time.time()
+    print(end - start)
+
 
 print(plot_all_lp_depol(100))
